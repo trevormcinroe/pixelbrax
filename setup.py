a64410569bd24bdee93a9244a679cb1ac2eb0e7a
trevor <t.mcinroe@ed.ac.uk>
2025-01-10 17:02:07 +0000

    adding renderer to codebase

---
+++ setup.py
@@ -0,0 +1,54 @@
+# Copyright 2024 The Brax Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""setup.py for PixelBrax.
+
+Install for development:
+
+  pip intall -e .
+"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="pixelbrax",
+    version="0.0.1",
+    description="...",
+    author="Trevor McInroe",
+    author_email="t.mcinroe@ed.ac.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/trevormcinroe/pixelbrax",
+    license="Apache 2.0",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=["bin/learn"],
+    install_requires=[
+        "trimesh",
+        "typing-extensions",
+    ],
+    extras_require={
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="JAX reinforcement learning rigidbody physics",
+)
+
